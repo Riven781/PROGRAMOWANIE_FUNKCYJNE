7d9d4de178d712969f2b95832cf7c051400c0c7d
Paweł Wódczyński <pawelwod123@gmail.com>
2025-10-23 22:38:37 +0200

    task3.5

--- ZADANIE1/src/main/scala/app/Zadanie1.scala
+++ ZADANIE1/src/main/scala/app/Zadanie1.scala
@@ -34,9 +34,37 @@ object Zadanie1 extends cask.MainRoutes{
     (0 until array.size - 1).forall(i => compare(array(i), array(i + 1)))
   }
 
- 
+  /*
+  3.5 Zaimplementuj funkcję, która zsumuje liczby po wierszach z trzech
+  list na wejściu; wykorzystaj funkcję sprowadzi funkcję z trzema
+  argumentami, do funkcji z dwoma
+  */
+
+  @cask.postJson("/sumLists")
+  def sumListsRequest(list1: List[Int], list2: List[Int], list3: List[Int]) = {
+    val resultList = sumLists(list1, list2, list3)
+    
+    ujson.Obj(
+      "sumLists" -> resultList
+    )
+  }
+
+  def sumLists(list1: List[Int], list2: List[Int], list3: List[Int]) : List[Int] = {
+    if (list1.isEmpty || list2.isEmpty || list3.isEmpty) return Nil
+    else{
+      val f3 = (a: Int, b: Int, c: Int) => a + b + c
+      val f2 = sumLists2(f3, list3.head)
+      f2(list1.head, list2.head) :: sumLists(list1.tail, list2.tail, list3.tail)
+    }
+   
+  }
+
+  def sumLists2(f3: (Int, Int, Int) => Int, value3: Int) : (Int, Int) => Int = {
+    (a, b) => f3(a, b, value3)
+  }
 
 
+  
   override def host: String = "0.0.0.0"
   initialize()
 }
