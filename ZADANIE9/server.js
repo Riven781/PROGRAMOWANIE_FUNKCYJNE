40d9312c124fed4e6a0fb0bb24ddad8d62e8ec56
Paweł Wódczyński <pawelwod123@gmail.com>
2026-01-06 13:13:20 +0100

    zad9.3.0

---
+++ ZADANIE9/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import { Worker } from 'worker_threads';
+
+const app = express();
+
+app.use(express.json());
+
+/*
+3.0 zwróci wartość binarną czy podana na wejściu liczba jest liczbą
+pierwszą; wykorzysta Promise
+*/ 
+
+function isPrime(num){
+  return new Promise((res, rej) => {
+    if (typeof num !== 'number'){
+      rej('Argument is not a number')
+    }
+    if (num < 2){
+      res(false)
+    }
+    for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++){
+      if (num % i === 0){
+        res(false)
+      }
+    }
+    res(true)
+  })
+}
+
+
+
+app.post('/isPrime', (req, res) => {
+  const {num} = req.body;
+  isPrime(num).then(result => {
+    res.json({result: result})}).catch(err => {
+      res.json({error: err})
+    });
+});
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
